832a3fb436b2b04db5a5eab79792eb69fb912312
Rawan El-Ghali <62294577+RawanHamza@users.noreply.github.com>
2023-02-22 01:38:06 +0200

    Create project2-Famous-people.sql

---
+++ project2-Famous-people.sql
@@ -0,0 +1,27 @@
+CREATE TABLE FAMOUS (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+name TEXT ,
+age INTEGER
+);
+INSERT INTO FAMOUS (name , age ) VALUES ('Ted Mosbey' , 30);
+INSERT INTO FAMOUS (name , age ) VALUES ('Robin' , 29);
+INSERT INTO FAMOUS (name , age ) VALUES ('Barney Stinson' , 31);
+INSERT INTO FAMOUS (name , age ) VALUES ('Lily Aldern' , 30);
+INSERT INTO FAMOUS (name , age ) VALUES ('Marshal Erikson' , 30);
+
+CREATE TABLE BESTFRIEND (
+id INTEGER PRIMARY KEY AUTOINCREMENT ,
+FAMOUS_id INTEGER
+,
+bffname TEXT
+);
+INSERT INTO BESTFRIEND ( FAMOUS_id, bffname) VALUES (1, 'Marshall Erikson');
+INSERT INTO BESTFRIEND ( FAMOUS_id, bffname) VALUES (2, 'Lily Aldern');
+INSERT INTO BESTFRIEND ( FAMOUS_id, bffname) VALUES (3, 'Ted Mosbey');
+INSERT INTO BESTFRIEND ( FAMOUS_id, bffname) VALUES (4, 'Robin');
+INSERT INTO BESTFRIEND ( FAMOUS_id, bffname) VALUES (5, 'Ted Mosbey');
+/*return eachone bestfriend*/
+SELECT FAMOUS.name , BESTFRIEND.bffname FROM
+FAMOUS
+JOIN BESTFRIEND 
+ON FAMOUS.id = BESTFRIEND.FAMOUS_id;
