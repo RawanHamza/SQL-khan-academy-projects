f6186f162645e88ec60e804a9303f8912f69978e
Rawan El-Ghali <62294577+RawanHamza@users.noreply.github.com>
2023-02-22 01:41:27 +0200

    Update project2-Famous-people.sql

--- project2-Famous-people.sql
+++ project2-Famous-people.sql
@@ -1,3 +1,4 @@
+/* Create table about the people and what they do here */
 CREATE TABLE FAMOUS (
 id INTEGER PRIMARY KEY AUTOINCREMENT,
 name TEXT ,
@@ -6,8 +7,8 @@ age INTEGER
 INSERT INTO FAMOUS (name , age ) VALUES ('Ted Mosbey' , 30);
 INSERT INTO FAMOUS (name , age ) VALUES ('Robin' , 29);
 INSERT INTO FAMOUS (name , age ) VALUES ('Barney Stinson' , 31);
-INSERT INTO FAMOUS (name , age ) VALUES ('Lily Aldern' , 30);
-INSERT INTO FAMOUS (name , age ) VALUES ('Marshal Erikson' , 30);
+INSERT INTO FAMOUS (name , age ) VALUES ('Lily Aldrin' , 30);
+INSERT INTO FAMOUS (name , age ) VALUES ('Marshall Erikson' , 30);
 
 CREATE TABLE BESTFRIEND (
 id INTEGER PRIMARY KEY AUTOINCREMENT ,
@@ -16,7 +17,7 @@ FAMOUS_id INTEGER
 bffname TEXT
 );
 INSERT INTO BESTFRIEND ( FAMOUS_id, bffname) VALUES (1, 'Marshall Erikson');
-INSERT INTO BESTFRIEND ( FAMOUS_id, bffname) VALUES (2, 'Lily Aldern');
+INSERT INTO BESTFRIEND ( FAMOUS_id, bffname) VALUES (2, 'Lily Aldrin');
 INSERT INTO BESTFRIEND ( FAMOUS_id, bffname) VALUES (3, 'Ted Mosbey');
 INSERT INTO BESTFRIEND ( FAMOUS_id, bffname) VALUES (4, 'Robin');
 INSERT INTO BESTFRIEND ( FAMOUS_id, bffname) VALUES (5, 'Ted Mosbey');
